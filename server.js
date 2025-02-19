df48c2d08c07dd5c240a2c838ed8c6dc52b36a3c
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-19 13:04:57 -0500

    added middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -19,7 +22,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,7 +31,10 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+
 app.get('/', (req, res) => {
+  console.log(res.locals.user);
   res.render('index.ejs', {
     user: req.session.user,
   });
@@ -36,6 +42,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/auth', authController);
+app.use(isSignedIn);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
