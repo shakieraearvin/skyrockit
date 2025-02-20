aeb93e533a2e4ea51193e24d752756234e4c43de
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-20 12:51:41 -0500

    add post route for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -27,3 +27,23 @@ router.get('/', async (req, res) => {
 router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
 });
+
+// POST /users/:userId/applications
+router.post('/', async (req, res) => {
+    try {
+        // Look up the user from req.session Or E
+        const currentUser = await User.findById(req.session.user._id);
+        // Push req.body (the new form data object) to the
+        // applications array of the current user or T
+        currentUser.applications.push(req.body);
+        // Save changes to the user or L 
+        await currentUser.save();
+        // Redirect back to the applications index view
+        res.redirect(`/users/${currentUser._id}/applications`);
+      } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
+      }
+
+});
