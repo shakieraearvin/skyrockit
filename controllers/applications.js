83eb8b1f0870f2e81b403075542ef64d26d276b2
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-20 12:17:00 -0500

    new route and new template

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,3 +8,22 @@ const User = require('../models/user.js');
 // we will build out our router logic here
 
 module.exports = router;
+
+
+// Routes for the application Schema
+// GET / users/userID/applications
+// Index route 
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+      } catch (error) {
+        console.log(error);
+        res.redirect('/');
+      }
+});
+
+// GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+});
--- server.js
+++ server.js
@@ -35,10 +35,11 @@ app.use(
 app.use(passUserToView);
 
 app.get('/', (req, res) => {
-  console.log(res.locals.user);
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  if (req.session.user) {
+    res.redirect(`users/$req.session.user._id}/applications`);
+  } else {
+    res.render('index.ejs');
+  }
 });
 
 
